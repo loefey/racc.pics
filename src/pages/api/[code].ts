e479804d812b0cdbaabc2bd64b1614c6bcd45ea7
venqoi <nolan@loefey.world>
2024-11-08 21:19:57 +0000

    Maybe fix error

--- src/pages/api/[code].ts
+++ src/pages/api/[code].ts
@@ -1,21 +1,25 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import path from 'path';
-import fs from 'fs';
+import { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+import fs from "fs";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const code = req.query.code as string;
 
-  const imagePath = path.join(process.cwd(), 'public', 'images', `${code}`);
+  const imagePath = path.join(process.cwd(), "public", "images", `${code}`);
 
   try {
     if (fs.existsSync(imagePath)) {
-      res.setHeader('Content-Type', 'image/png');
+      res.setHeader("Content-Type", "image/png");
       const image = fs.readFileSync(imagePath);
       res.status(200).send(image);
     } else {
-      res.status(404).json({ error: 'Image not found' });
+      res.status(404).json({ error: "Image not found" });
     }
   } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
+    console.error("Error reading image file:", error); // Log the error
+    res.status(500).json({ error: "Internal server error" });
   }
 }
